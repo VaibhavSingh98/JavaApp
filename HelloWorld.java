a28cb73d53557687423c75de3311e09f3f5a2039
Pradeep771 <sonupradeeppsp@gmail.com>
2020-03-30 15:10:14 +0530

    commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,8 @@ public class HelloWorld
 {   
 	public static void main(String args[])
 	{
-		System.out.println("HelloWorld");
+		System.out.println("HelloWorld Pradeep");
 		System.out.println("commit");
-
+                 
 	}
-}
+} 
